46fa0b56980bfc39af75a94c79b896fa1350a7c1
iregbnz <42605336+iregbnz@users.noreply.github.com>
2018-11-26 21:15:04 +0400

    Add files via upload

---
+++ Ruby/gigasecond/gigasecond.rb
@@ -0,0 +1,6 @@
+class Gigasecond
+   def self.from(time)
+     "Enter #{time} for me."
+     time +10**9
+  end
+end
---
+++ Ruby/leap/leap.rb
@@ -0,0 +1,7 @@
+
+module Year
+  def self.leap?(date="2015")
+    "Enter #{date} for me."
+    date = date/4
+  end
+end
